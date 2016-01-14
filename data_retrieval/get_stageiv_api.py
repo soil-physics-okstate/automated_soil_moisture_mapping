a7cb676112364898e851cb1cc3cb06edc95b983d
Jason Patton <jason.c.patton@okstate.edu>
2016-01-14 11:13:14 -0600

    Grab API data and include it in the regression

---
+++ data_retrieval/get_stageiv_api.py
@@ -0,0 +1,88 @@
+from sys import argv
+from datetime import datetime, timedelta
+import numpy as np
+from urllib import urlretrieve
+from netCDF4 import MFDataset
+from pandas import DataFrame
+from os.path import isfile
+
+api_params = {
+    05: {'days': 24, 'k': 1.025},
+    25: {'days': 24, 'k': 1.006},
+    60: {'days': 22, 'k': 0.993}
+}
+nd = 24
+
+date_in = argv[1] # current date passed in as yyyy-mm-dd
+date = datetime.strptime(date_in, '%Y-%m-%d') # convert to a datetime object
+
+print 'Getting StageIV antecedent precipitation indicies for %s...' % (date_in)
+
+# 'date' is Current Date, 0000 UTC
+# midnight is at Current Date, 0600 UTC
+date = date + timedelta(hours=6)
+
+url = 'http://www.mesonet.org/data/public/noaa/qpe/abrfc/1hr_netcdf/%04d/%02d/%02d/%s.nc'
+# url takes (year, month, day, t_str)
+
+# set data_dir
+data_dir = '../data/'
+
+# location to store NetCDF files temporarily
+nc_dir = data_dir + 'precip/stageiv_1h_qpe_netcdf/'
+
+# NetCDF files' valid times include precip summed over the previous hour
+# so data collection should stop at 0500 UTC
+nc_hours = range(1, 24*nd + 1) # will not include 0600!
+nc_dh = np.array([timedelta(hours = h) for h in nc_hours])
+
+# get list of times and start a list of files
+nc_times = date - nc_dh
+nc_files = []
+
+# loop over all times
+for t in nc_times:
+
+    # get the time string
+    t_str = t.strftime('%Y%m%d%H') # yyyymmddHH
+
+    # get the filename and download the file to local storage
+    nc_file = nc_dir + t_str + '.nc'
+    if not isfile(nc_file): # check if file already exists
+        urlretrieve(url % (t.year, t.month, t.day, t_str), nc_file)
+    nc_files.append(nc_file)
+
+# load the files and pull the precip data
+nc = MFDataset(nc_files, aggdim='time')
+precip = nc.variables['Total_precipitation'][:]
+nc.close()
+
+# accumulate daily precip
+daily_precip = precip.reshape((24, nd, 159, 335)).sum(axis=0)
+
+api_df = []
+# calculate the antecedent precipitation indicies
+for depth in sorted(api_params.keys()):
+    scales = api_params[depth]['k']**np.arange(0, -api_params[depth]['days'], -1)
+    api_components = []
+    for i,scale in enumerate(scales):
+        api_components.append(daily_precip[i]*scale)
+    api = np.array(api_components).sum(axis=0)
+
+    temp_df = DataFrame(api)
+    temp_df.columns.name = 'x'
+    temp_df.index.name = 'y'
+    temp_df = temp_df.stack().swaplevel('x', 'y').sort_index()
+    temp_df.name = depth
+    temp_df = DataFrame(temp_df)
+    
+    if len(api_df) == 0:
+        api_df = temp_df
+    else:
+        api_df = api_df.join(temp_df)
+    
+# save precipitation to CSV
+out_dir = data_dir + 'precip/stageiv_api/'
+api_df.to_csv(out_dir + 'api_%s.csv' % (date.strftime('%Y%m%d')))
+
+print '  Done.'
--- regression/do_regression.py
+++ regression/do_regression.py
@@ -20,27 +20,27 @@ sand_df = pickle.load(open(data_dir + 'soil_properties/ssurgo/sand_content.pickl
 
 # load dynamic (CSV) data sources
 
-precip_5day_file = data_dir + 'precip/stageiv_5_day/5_day_precip_%s.csv' % (date_str)
-precip_30day_file = data_dir + 'precip/stageiv_30_day/30_day_precip_%s.csv' % (date_str)
+api_file = data_dir + 'precip/stageiv_api/api_%s.csv' % (date_str)
 sm_file = data_dir + 'soil_moisture/midnight/sm_data_%s.csv' % (date_str)
 
 from pandas import read_csv
 
-precip_5day_df = read_csv(precip_5day_file, names=['x','y','precip_5day']).set_index(['x','y'])
-precip_30day_df = read_csv(precip_30day_file, names=['x','y','precip_30day']).set_index(['x','y'])
+api_df = read_csv(api_file).set_index(['x', 'y'])
 sm_df = read_csv(sm_file, header=[0,1], index_col=0)
 
+# need to fix the api DataFrame columns to have names
+depths = [5, 25, 60]
+api_df.columns = ['api_%d' % (d) for d in depths]
+
 # need to fix the soil moisture DataFrame columns to have integer depths
-# the levels are sorted by string value, which is why 25 comes first
+# the levels in a MultiIndex are sorted by string value, which is why 25 comes first
 sm_df.columns.set_levels(level=1, levels=[25,5,60], inplace=True)
 
 # combine data
 df = meso_df.join(sand_df, on='mukey')\
             .join(sm_df['vwc'], lsuffix='_sand', rsuffix='_vwc')\
-            .join(precip_5day_df, on=['s4x', 's4y'])\
-            .join(precip_30day_df, on=['s4x', 's4y'])
+            .join(api_df, on=['s4x', 's4y'])\
 
-depths = [5, 25, 60]
 for d in depths: # OLS function cannot handle leading numbers in variables
     df.rename(inplace=True, columns={'%d_vwc' % (d): 'vwc_%d' % (d),
                                      '%d_sand' % (d): 'sand_%d' % (d)})
@@ -54,7 +54,7 @@ results = {}
 
 # do the fitting for each depth
 for d in depths:
-    formula = 'vwc_%d ~ x + y + sand_%d + precip_5day + precip_30day' % (d, d)
+    formula = 'vwc_%d ~ sand_%d + api_%d' % (d, d, d)
     results[d] = smf.ols(formula, data=df).fit()
 
     # save the fitted values and residual values to the dataframe
