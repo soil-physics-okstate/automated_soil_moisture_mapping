7a9967d02cb5d507443ef37b8dd6ddec4339db5c
Jason Patton <jason.c.patton@okstate.edu>
2016-01-28 15:05:55 -0600

    Better commenting

--- data/soil_properties/meso_soil/pickle_mesosoil.py
+++ data/soil_properties/meso_soil/pickle_mesosoil.py
@@ -1,9 +1,21 @@
 from pandas import read_excel
 
+# input file
 fname = 'MesoSoilv1_3.xlsx'
 
+# skip the first row (describes file) and third row (describes units)
+# and turn missing data (-9.9) into NaNs
 df = read_excel(fname, header=0, skiprows=[0,2], na_values=['-9.9'])
-df.columns = [c.lower() for c in df.columns]
-df = df.rename(columns={'site': 'STID'}).set_index(['STID', 'depth']).unstack()
 
+# make the columns use lowercase
+df.columns = [c.lower() for c in df.columns]
+
+# rename the "site" column to the more standard "STID" that's used elsewhere
+df = df.rename(columns={'site': 'STID'})
+
+# set STID and depth as the index and unstack the depth index
+# so that it is a column index
+df = df.set_index(['STID', 'depth']).unstack()
+
+# save the DataFrame in a pickle named the same as the input file
 df.to_pickle(fname.replace('xlsx', 'pickle'))
