4eb8ae18ec9532bf0c259b45459b73d5c07c798c
Jason Patton <jason.c.patton@okstate.edu>
2016-02-03 14:24:43 -0600

    Include state and county shapefile manipulation in repository

---
+++ gis/state_county_shapefiles/build_shapefiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# download US counties shapefile
+wget http://www2.census.gov/geo/tiger/TIGER2014/COUNTY/tl_2014_us_county.zip -O tl_2014_us_county.zip
+
+# unzip
+unzip -f tl_2014_us_county.zip -d tl_2014_us_county
+
+# extract Oklahoma counties into their own shapefile
+input=tl_2014_us_county
+output=tl_2014_oklahoma_county
+mkdir -p $output
+ogr2ogr -overwrite -where 'STATEFP = "40"' $output/$output.shp $input/$input.shp
+
+# dissolve Oklahoma counties into a state shapefile
+input=tl_2014_oklahoma_county
+output=tl_2014_oklahoma_state
+mkdir -p $output
+ogr2ogr -overwrite $output/$output.shp $input/$input.shp -dialect sqlite -sql "SELECT ST_Union(geometry,geometry), STATEFP FROM $input GROUP BY STATEFP"
