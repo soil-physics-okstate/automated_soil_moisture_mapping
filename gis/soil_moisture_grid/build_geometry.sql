274c69fadef039072ceca97023c3735c63601c52
Jason Patton <jason.c.patton@okstate.edu>
2016-02-04 15:56:59 -0600

    Shuffle soil moisture grid info

--- gis/soil_moisture_grid/build_geometry.sql
+++ gis/soil_moisture_grid/build_geometry.sql
@@ -1,8 +1,3 @@
--- add longitude and latitude columns
-ALTER TABLE soil_moisture_grid_id 
-      ADD longitude DOUBLE PRECISION, 
-      ADD latitude DOUBLE PRECISION;
-
 -- add point and pixel geometry columns
 SELECT AddGeometryColumn('soil_moisture_grid_id', 'geom_point', 4326, 'POINT', 2);
 SELECT AddGeometryColumn('soil_moisture_grid_id', 'geom_pixel', 4326, 'POLYGON', 2);
@@ -15,6 +10,3 @@ UPDATE soil_moisture_grid_id SET
 UPDATE soil_moisture_grid_id SET
        geom_pixel = ST_Transform( ST_Expand( ST_Transform(geom_point, 990001), 400 ), 4326);
 
--- get longitudes and latitudes from geometry
-UPDATE soil_moisture_grid_id SET
-       longitude = ST_X(geom_point), latitude = ST_Y(geom_point);
