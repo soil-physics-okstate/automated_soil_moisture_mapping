108c315c016d4935750537481ee8ebecd30bdcbb
Jason Patton <jason.c.patton@okstate.edu>
2016-02-16 14:43:17 -0600

    Remove old SQL (exists in gis_setup now)

--- database_scripts/add_geometry_soil_moisture_grid_id.sql
+++
@@ -1,8 +0,0 @@
-SELECT AddGeometryColumn('soil_moisture_grid_id', 'geom_point', 4326, 'POINT', 2);
-SELECT AddGeometryColumn('soil_moisture_grid_id', 'geom_pixel', 4326, 'POLYGON', 2);
-
-UPDATE soil_moisture_grid_id
-       SET geom_point = ST_SetSRID(ST_MakePoint(longitude, latitude), 4326);
-
-UPDATE soil_moisture_grid_id 
-       SET geom_pixel = ST_Transform(ST_Expand(ST_Transform(geom_point, 990001), 400), 4326);
--- database_scripts/create_table_soil_moisture_grid_id.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE soil_moisture_grid_id (
-       id INTEGER PRIMARY KEY,
-       x INTEGER NOT NULL,
-       y INTEGER NOT NULL,
-       longitude DOUBLE PRECISION NOT NULL,
-       latitude DOUBLE PRECISION NOT NULL
-);
