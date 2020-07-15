97b5ff0fbf982c355d23338565c55edb118da828
Evan Linde <elinde@okstate.edu>
2020-07-14 19:41:37 -0500

    add scripts for exporting v1.1.0 data

---
+++ netcdf_export/v1.1.0_monthly_netcdf.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Create/rewrite netCDF file of VWC data for the given month
+
+month="$1"  # e.g. "2018-07"
+
+version="1.1.0"
+input_raster_dir="/opt/soilmapnik/v1.1_data/output/raw_rasters"
+input_model_dir="/opt/soilmapnik/v1.1_data/output/semivariogram/model"
+input_rmse_dir="/opt/soilmapnik/v1.1_data/output/kriging_cross_validation"
+#output_netcdf_dir="/opt/soilmapnik/v1.1_data/output/netcdf/monthly"
+output_netcdf_dir="/opt/soilmapnik/output/netcdf/v${version}/monthly"
+venv="/opt/soilmapnik/automated_soil_moisture_mapping/venv/bin/activate"
+
+if [[ "${month}" == "$(date +%Y-%m)" ]]; then
+  num_days=$(($(date +%d) - 1))
+else
+  num_days=$(($(date --date="${month}-01 +1 month -1 day" +%d) - 1))
+fi
+
+datestr=$(for ((day=0; day<=${num_days}; day++)); do echo "${month}-01 +${day} days"; done)
+dates=$(date -f - +"%Y-%m-%d" <<< "${datestr}")
+
+source ${venv}
+ncfile="vwc_v${version}_${month}.nc"
+python vwc_time_series_to_netcdf.py ${ncfile} ${version} "${input_raster_dir}" "${input_model_dir}" "${input_rmse_dir}" ${dates}
+deactivate
+
+cp ${ncfile} ${output_netcdf_dir}/ && rm ${ncfile}
+
